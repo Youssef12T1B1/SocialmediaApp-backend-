6a26680ae402113dcf8d0b64af116e9a71d43f8d
Youssef12T1B1 <yousseattabi1998@gmail.com>
2022-03-27 22:05:22 +0100

    start

--- server/index.js
+++ server/index.js
@@ -3,6 +3,11 @@ const typeDefs = require("./graphql/typedefs");
 const resolvers = require("./graphql/resolvers");
 
 const server = new ApolloServer({
-  typeDefs: typeDefs,
-  resolvers: resolvers,
+  typeDefs,
+  resolvers,
 });
+
+server
+  .listen(4000)
+  .then(({ url }) => console.log(`Serveris  running on ${url}`))
+  .catch((err) => console.log(err));
